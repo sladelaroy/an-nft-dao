2c9edae60a598818465ca0e388175d6359d171fc
sladelaroy <fuunshofaleye1@gmail.com>
2025-01-06 09:32:15 +0100

    removed some vulnerablities

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: path.resolve(__dirname, './.env') });
 
-const MNEMONIC = "purpose story artwork beyond execute west bless kingdom fine much affair planet";
-
-const INFURA_PROJECT_ID = "4e6abfe74ec3487da3f227fbcd7ba61d";
 // const { MNEMONIC, INFURA_PROJECT_ID } = {"purpose story artwork beyond execute west bless kingdom fine much affair planet", };
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -12,8 +9,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: { mnemonic: MNEMONIC }
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: { mnemonic: process.env.MNEMONIC }
     }
   },
   // etherscan: {
